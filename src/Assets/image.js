5c1e71636387d0d2e58cd6545937e5b2c3a5b281
kareraolivier <kareraolivier@gmail.com>
2022-09-19 19:15:15 +0200

    finish

--- src/Assets/image.js
+++ src/Assets/image.js
@@ -3,6 +3,12 @@ import homeimg from "./home.jpeg";
 import ai1 from "./AI1.jpeg";
 import ai2 from "./AI2.jpeg";
 import ai3 from "./AI3.jpeg";
+import a from "./a.jpeg";
+import b from "./b.jpeg";
+import c from "./c.jpg";
+import d from "./d.jpeg";
+import e from "./e.jpeg";
+import f from "./f.jpeg";
 
 export default {
   logo,
@@ -10,4 +16,10 @@ export default {
   ai1,
   ai2,
   ai3,
+  a,
+  b,
+  c,
+  d,
+  e,
+  f,
 };
--- src/container/About.js
+++ src/container/About.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styles from "../../styles/About.module.scss";
 import Button from "../components/Button";
+import Image from "next/image";
+import { images } from "../Assets";
 const About = () => {
   return (
     <div id="Aboutus">
@@ -47,22 +49,28 @@ const About = () => {
           </div>
           <div className={styles.about_lower_image}>
             <div className={styles.image1}>
-              <h2>
-                Fast and easy intergration no <br />
-                coding needed
-              </h2>
+              <div className={styles.smallimage}>
+                <Image src={images.a} alt="a" width="100%" height="100%" />
+              </div>
+              <div className={styles.smallimage}>
+                <Image src={images.b} alt="b" width="100%" height="100%" />
+              </div>
             </div>
             <div className={styles.image2}>
-              <h2>
-                Fast and easy intergration no
-                <br /> coding needed
-              </h2>
+              <div className={styles.smallimage}>
+                <Image src={images.c} alt="c" width="100%" height="100%" />
+              </div>
+              <div className={styles.smallimage}>
+                <Image src={images.d} alt="d" width="100%" height="100%" />
+              </div>
             </div>
             <div className={styles.image3}>
-              <h2>
-                Fast and easy intergration no
-                <br /> coding needed
-              </h2>
+              <div className={styles.smallimage}>
+                <Image src={images.e} alt="e" width="100%" height="100%" />
+              </div>
+              <div className={styles.smallimage}>
+                <Image src={images.f} alt="f" width="100%" height="100%" />
+              </div>
             </div>
           </div>
         </div>
