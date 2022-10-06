14c6a6c0025f9d6665d92d8ccd9e8d0875a59e5b
kareraolivier <kareraolivier@gmail.com>
2022-10-06 14:26:23 +0200

    updates

--- src/container/Footer.js
+++ src/container/Footer.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Image from "next/image";
+import { images } from "../Assets";
 import styles from "../../styles/Footer.module.scss";
 import Button from "../components/Button";
 const Footer = () => {
@@ -17,26 +19,43 @@ const Footer = () => {
           <div className={styles.works_button}>
             <Button
               text="Book a demo"
-              color="#252526"
-              bcolor="white"
+              color="#FFFFFF"
+              bcolor="#FF645A"
               width="10rem"
-              border="solid #252526"
+              border="none"
             />
           </div>
         </div>
         <div className={styles.footer_lower}>
           <div>
-            <h3>PRODUCT</h3>
+            <h3>INTERGRATIONs</h3>
+            <p>magento</p>
+            <p>woocommerce</p>
+            <p>Google tag manager</p>
+            <p>Custom implementation</p>
           </div>
           <div>
             <h3>INTERGRATION</h3>
-          </div>
-          <div>
-            <h3>RESOURCES</h3>
+            <p>Product recommendation</p>
+            <p>size helper</p>
+            <p>funnel helper</p>
           </div>
           <div>
             <h3>ABOUT US</h3>
+            <p>How it started</p>
+            <p>Company info</p>
+            <p>term</p>
+            <p>privacy</p>
           </div>
+          <div>
+            <h3>CONTACT US</h3>
+            <p>Dr. van deenweg 151 8011HJ Zwolle</p>
+            <p>+31(0) 933 02 292</p>
+            <p>info@chosify.com</p>
+          </div>
+        </div>
+        <div className={styles.lastlogo}>
+          <Image src={images.logo} alt="footerlogo" width={150} height={60} />
         </div>
       </div>
     </div>
